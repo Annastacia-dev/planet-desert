cc4eb997d1a452eb8b21f2516be7cb96d8ececee
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2023-04-11 21:13:20 +0300

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./Navbar"
+
+function App() {
+  return (
+   <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <Navbar />
+   </main>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors : {
+        brunswick: '#264A3E',
+        celadon: '#B0D099',
+        gunmetal:'#213032',
+        asparagus: '#6B9059',
+        fern: '#497551',
+      },
+      fontFamily: {
+        rubik : ['Rubik', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
